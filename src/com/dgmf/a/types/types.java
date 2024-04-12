2b1a65582c41ad26a82c7291917f7dc3b5f4441e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-12 04:22:06 +0200

    Primitive Types

---
+++ src/com/dgmf/a/types/types.java
@@ -0,0 +1,15 @@
+package com.dgmf.a.types;
+
+public class types {
+    public static void main(String[] args) {
+        /* Primitive Types */
+        byte myNumber = 5;
+        int viewsCount = 123_456_789;
+        // int viewsCount = 3_123_456_789; // Error => Integer Number Too Large
+        long longViewsCount = 3_123_456_789L;
+        float price = 147.99F;
+        double doubleViewsCount = 3_123_456_789D; // Or "3_123_456_789."
+        char letter = 'B';
+        boolean isActive = true; // Or "false;
+    }
+}
